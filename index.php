e7e5ccb82bad7573b4f944eda413223cc0940e8e
Bond <bond-d@ukr.net>
2020-08-23 07:24:44 +0300

    123

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-print 'ti pes';
-file_put_contents('pes.txt', 'pes');
+require __DIR__ . '/vendor/autoload.php';
+use Kreait\Firebase\Factory;
+
+$servername = "f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+$username = "wrdo12m7ecr8u77p";
+$password = "password";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=fdg8hfjnr3bnca2t", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+$lastDate = $conn->query('SELECT value FROM value WHERE id =1');
+print_r($lastDate);
